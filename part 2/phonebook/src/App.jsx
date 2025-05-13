69ef349f66175d358ea69c381a9b4621bcfaa218
John Chun <johnchun@mac.lan>
2025-05-13 09:14:16 -0700

    exercises 2.8-2.10

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
-
+  const [filterSearch, setFilterSearch] = useState('')
   const addName = (e) => {
     setNewName(e.target.value)
   }
@@ -19,9 +22,17 @@ const App = () => {
     }
   }
 
+  const filterSubmit = (e) => {
+    e.preventDefault
+    console.log(e.target.value)
+    setFilterSearch(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={filterSearch} type="text" onChange={filterSubmit}/>
+    {persons.some(person => person.name.toLowerCase() === filterSearch.toLowerCase()) ? filterSearch : ''}
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName}  onChange={addName}/>
