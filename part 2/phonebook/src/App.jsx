d5b020b5c7c3adc062ccaef437f5a882ac489b60
John Chun <johnchun@mac.lan>
2025-05-14 09:36:26 -0700

    exercises 2.12-2.15

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import {nanoid} from 'nanoid'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [filterSearch, setFilterSearch] = useState('')
+
   const addName = (e) => {
     setNewName(e.target.value)
   }
@@ -14,7 +16,7 @@ const App = () => {
       .then(res => setPersons(res.data))
       .catch(err => console.error('Error:', err))
 
-   })
+   },[])
 
 
   const handleSubmit = (e) => {
@@ -22,7 +24,18 @@ const App = () => {
     if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase())){
       alert(`${newName} is already added to phonebook `)
     } else { 
-      setPersons([...persons, {name: newName} ])
+      const newPerson = {
+        name: newName,
+        id: nanoid()
+      }
+      axios.post('http://localhost:3001/persons', newPerson)
+        .then(res => {
+          console.log('Response:', res.data)
+          setPersons([...persons, res.data])
+          setNewName('')
+        })
+        .catch(err => console.error('Error:', err))
+      
     }
   }
 
@@ -30,6 +43,21 @@ const App = () => {
     e.preventDefault
     console.log(e.target.value)
     setFilterSearch(e.target.value)
+  }
+
+  const deletePerson = (id) => {
+    const findPerson = persons.filter(person => person.id === id)[0]
+    console.log(findPerson)
+    const name = findPerson.name
+    console.log(name)
+    if (window.confirm(`Delete ${name}?`)){axios
+      .delete(`http://localhost:3001/persons/${id}`)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
+      .catch(err => {
+        console.error('Failed to delete contact', err)
+      })}
   }
 
   return (
@@ -46,7 +74,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <h2>{person.name}</h2>)}
+      {persons.map(person => {
+        return (
+        <>
+        <h2>{person.name}</h2>
+        <button onClick={() => deletePerson(person.id)}>delete</button>
+        </>
+        )
+      })}
     </div>
   )
 }
